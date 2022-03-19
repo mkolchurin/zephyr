31cfdd9aac761cf51c28a5eaccb335e5ee2eedc3
Maxim Kolchurin <maxim.kolchurin@gmail.com>
2022-03-19 12:48:58 +0300

    drivers: eeprom-flash fix

--- drivers/flash/flash_eeprom.c
+++ drivers/flash/flash_eeprom.c
@@ -13,11 +13,12 @@ static const struct flash_parameters emul_parameters = {
 	.erase_value = 0xff,
 };
 
-static const struct flash_pages_layout flash_layout[] = { { .pages_count = 32,
-							    .pages_size = 512 } };
+static struct flash_pages_layout flash_layout = { .pages_count = 0, .pages_size = 0 };
 
 struct flash_eeprom_emul_config {
 	struct device *eeprom;
+	size_t page_size;
+	size_t page_count;
 };
 
 static int emul_init(const struct device *dev)
@@ -61,10 +62,12 @@ static int emul_erase(const struct device *dev, off_t offset, size_t size)
 void emul_page_layout(const struct device *dev, const struct flash_pages_layout **layout,
 		      size_t *layout_size)
 {
-	ARG_UNUSED(dev);
+	const struct flash_eeprom_emul_config *config = dev->config;
+	flash_layout.pages_count = config->page_count;
+	flash_layout.pages_size = config->page_size;
 
-	*layout = flash_layout;
-	*layout_size = ARRAY_SIZE(flash_layout);
+	*layout = &flash_layout;
+	*layout_size = 1;
 }
 
 static const struct flash_parameters *emul_get_parameters(const struct device *dev)
@@ -82,11 +85,15 @@ static const struct flash_driver_api emul_api = {
 	.page_layout = emul_page_layout,
 };
 
+#define EEPROM_DEVICE(n) DEVICE_DT_GET(DT_INST_PARENT(n))
+
 #define FLASH_EEPROM_DEF(n)                                                                        \
 	struct flash_eeprom_emul_config config_##n = {                                             \
-		.eeprom = DEVICE_DT_GET(DT_INST_PARENT(n)),                                           \
+		.eeprom = EEPROM_DEVICE(n),                                                        \
+		.page_size = DT_INST_PROP(n, page_size),                                           \
+		.page_count = DT_INST_PROP(n, page_count),                                         \
 	};                                                                                         \
-	DEVICE_DT_INST_DEFINE(n, emul_init, NULL, NULL, &config_##n, POST_KERNEL,                 \
-			 CONFIG_KERNEL_INIT_PRIORITY_DEVICE, &emul_api);
+	DEVICE_DT_INST_DEFINE(n, emul_init, NULL, NULL, &config_##n, POST_KERNEL,                  \
+			      CONFIG_KERNEL_INIT_PRIORITY_DEVICE, &emul_api);
 
 DT_INST_FOREACH_STATUS_OKAY(FLASH_EEPROM_DEF)
